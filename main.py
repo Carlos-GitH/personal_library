524d96cee56eab5f2109c08d397adfef22e2640c
Carlos Aluizio G. Cavalcanti Filho <carlosagcf@outlook.com>
2023-06-16 09:28:24 -0300

    Now the books information is stored in a database

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
-# import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+# import sqlite3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
-
 db = SQLAlchemy(app)
 
+#CREATES THE CLASS FOR THE TABLE ON ALCHEMY
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
+
+#CREATES THE DATABASE AND ADD FIRST REGISTRY
 # with app.app_context():
 #     db.create_all()
 #     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
 #     db.session.add(new_book)
 #     db.session.commit()
-all_books = []
 
-
-
+#CREATES THE DATABASE WITH SQLITE3
 # db = sqlite3.connect('books-collection.db')
 # cursor = db.cursor()
 # cursor.execute("""CREATE TABLE books (
@@ -30,27 +30,32 @@ all_books = []
 #                     author varchar(250) NOT NULL,
 #                     rating FLOAT NOT NULL)""")
 
+#CREATES FIRST REGISTRY ON THE DATABASE WITH SQLITE3
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
+#HOME ROUTE
 @app.route('/')
 def home():
-    print('dauodhawu')
-    return render_template('index.html', books=all_books)
+#QUERY ALL THE DATA IN THE DATABESE TO SHOW ON THE HOMEPAGE    
+    with app.app_context():
+        all_books = db.session.execute(db.select(Book)).scalars()
+        return render_template('index.html', books=all_books)
 
-
+#ROUTE FOR ADDING NEW BOOKS
 @app.route("/add", methods=['GET', 'POST'])
+#FUNCTION THAT CAPTURES THE VALUES FILLED IN THE FORM AND ADD TO THE DATABASE
 def add():
     if request.method == 'POST':
-        new_book = {
-            'book': request.form['book'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books.append(new_book)
-        print(all_books)
+        new_book = Book(
+            title  = request.form['book'],
+            author = request.form['author'],
+            rating = request.form['rating']
+        )
+        with app.app_context():
+            db.session.add(new_book)
+            db.session.commit()        
         return redirect(url_for('home'))
-    print(all_books)
     return render_template('add.html')
 
 
